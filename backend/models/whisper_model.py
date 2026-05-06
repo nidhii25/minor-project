b9677c96c0a1a431e6b3a789fe050bc595c5afe2
nidhi <devnidhi15@gmail.com>
2026-05-06 11:23:23 +0530

    optimized whisper model for deployment

--- backend/models/whisper_model.py
+++ backend/models/whisper_model.py
@@ -1,7 +1,16 @@
 import whisper
 
-model = whisper.load_model("base")
+model = None
+
+def get_model():
+    global model
+
+    if model is None:
+        model = whisper.load_model("tiny")
+
+    return model
 
 def transcribe_audio(file_path):
+    model = get_model()
     result = model.transcribe(file_path, word_timestamps=True)
     return result
